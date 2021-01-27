d36180a33a4ff08a608111a5ebf17e43fb2c6ff2
Enu Azubike <enuazubike88@gmail.com>
2021-01-27 14:30:00 +0100

    password encryption

--- index.js
+++ index.js
@@ -4,20 +4,30 @@
 
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
+const cookieSession = require( "cookie-session" );
+const usersRepo = require( "./repositories/users" );
+const e = require( "express" );
 const app = express();
+
 
 // make the middleware global
 app.use( bodyParser.urlencoded( { extended: true } ) );
-
+app.use( cookieSession( {
+    keys: ['kggfhhytwwttwuudhbxvfeettruzxcxv']
+} ) )
 
 // add a route handler 
-app.get( '/', ( req, res ) => {
+app.get( '/signup', ( req, res ) => {
     res.send( `
+        Your ID is  ${req.session.userId}
         <form method ="post">
         Email : <input type="text" placeholder="email" name = "email"/>
         <br/>
         <br/>
         Password : <input type="password" placeholder="password" name ="password"/>
+        <br/>
+        <br/>
+        Confirm Password : <input type="password" placeholder="confirm password" name ="passwordConfirmation"/>
         <br/>
         <br/>
         <button> SignUp</button>
@@ -27,13 +37,71 @@ app.get( '/', ( req, res ) => {
 } );
 
 
-app.post( '/', ( req, res ) => {
+app.post( '/signup', async ( req, res ) => {
 
     // get access to email and password 
-    console.log( req.body )
+    const { email, password, passwordConfirmation } = req.body;
+
+    const isExistingUser = await usersRepo.getOneBy( { email } );
+
+    // perform validation on email and password 
+    if ( isExistingUser ) {
+        return res.send( "Email already in use!!!" );
+    }
+    if ( password !== passwordConfirmation ) {
+        return res.send( "Passwords must match" );
+    }
+
+    //create user in our user repository that represents the user 
+    const user = await usersRepo.create( { email, password } );
+    // store the id inside the users cookie  ==> req.session is added by the cookie-session 
+    req.session.userId = user.id;
     res.send( ` account created !!!` );
 } );
 
+//signout
+app.get( '/signout', ( req, res ) => {
+    req.session = null;
+    res.send( "You are logged out!!" );
+} )
+
+// signin 
+
+app.get( '/signin', ( req, res ) => {
+    res.send( `
+    <form method ="post">
+    Email : <input type="text" placeholder="email" name = "email"/>
+    <br/>
+    <br/>
+    Password : <input type="password" placeholder="password" name ="password"/>
+    <br/>
+    <br/>
+    <button> Sign In</button>
+    </form>
+
+`)
+
+} )
+
+
+app.post( '/signin', async ( req, res ) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy( { email } );
+    if ( !user ) {
+        res.send( "Email not found!!!" );
+    }
+    const validPassword = await usersRepo.comparePassword( user.password, password );
+    if ( !validPassword ) {
+        res.send( "Invalid password!!!" );
+    }
+    req.session.userId = user.id;
+
+    res.send( "You are signed in " );
+
+
+} )
+
+// watch for incoming requests on port 3000!
 app.listen( 3000, () => {
     console.log( 'App listening on port 3000!' );
 } );
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 
 const fs = require( 'fs' );
+const crypto = require( 'crypto' );
+const util = require( 'util' );
+const scrypt = util.promisify( crypto.scrypt );
 
 class UsersRepository {
     constructor( fileName ) {
@@ -20,14 +23,79 @@ class UsersRepository {
         // read the file
         return JSON.parse( await fs.promises.readFile( this.fileName, { encoding: "utf-8" } ) );
     }
+    async writeAll ( records ) {
+        await ( fs.promises.writeFile( "users.json", JSON.stringify( records ), { encoding: "utf8" } ) );
+    }
 
+
+    async create ( attribute ) {
+        // read the an extract the array 
+        attribute.id = this.randomId();
+        const salt = crypto.randomBytes( 8 ).toString( 'hex' );
+        const buff = await scrypt( attribute.password, salt, 64 )
+        const users = await this.getAll();
+        const record = { ...attribute, password: `${buff.toString( 'hex' )}.${salt}` };
+        // add the object to an array 
+        users.push( record );
+        // write to the file system 
+        await this.writeAll( users );
+        return record;
+
+    }
+
+    async comparePassword ( savedPassword, suppliedPassword ) {
+        const [hashed, salt] = savedPassword.split( "." );
+        const buff = await scrypt( suppliedPassword, salt, 64 );
+        return hashed === buff.toString( 'hex' );
+    }
+
+    randomId () {
+        return crypto.randomBytes( 4 ).toString( 'hex' );
+
+    }
+
+    async findById ( id ) {
+        const users = await this.getAll()
+        return users.find( user => user.id === id );
+    }
+
+    async delete ( id ) {
+        const users = await this.getAll();
+        const filterdRecords = users.filter( user => user.id !== id );
+        await this.writeAll( filterdRecords );
+    }
+
+    async update ( id, attribute ) {
+        const users = await this.getAll();
+        const user = users.find( user => user.id === id );
+        if ( !user ) {
+            throw new Error( `Record with id : ${id} is not found` );
+        }
+
+        Object.assign( user, attribute );
+        await this.writeAll( users );
+
+    }
+
+    async getOneBy ( filters ) {
+        const users = await this.getAll();
+        for ( const user of users ) {
+            let found = true;
+            for ( const key in filters ) {
+                if ( user[key] !== filters[key] ) found = false;
+            }
+
+            if ( found == true ) {
+                return user;
+            }
+        }
+
+    }
 
 }
 
-const test = async () => {
-    const repo = new UsersRepository( "users.json" );
-    const users = await repo.getAll();
-    console.log( users );
-}
 
-test();
+
+
+module.exports = new UsersRepository( 'users.json' );
+
